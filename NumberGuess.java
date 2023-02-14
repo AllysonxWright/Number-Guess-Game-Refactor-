1e28adf114e7a30755bf627bbcf774d1f8d8cdaa
AllysonxWright <114745945+AllysonxWright@users.noreply.github.com>
2023-02-13 21:50:51 -0500

    Add files via upload

---
+++ NumberGuess.java
@@ -0,0 +1,29 @@
+// I added difficulties 
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class NumberGuess {
+    public static void main(String[] args) {
+
+            Game gz = new Game (1);
+            Game gf = new Game (2);
+            Game gc = new Game (3);
+        }
+            
+
+
+        }
+
+
+/*
+ * Why I chose not to let the user choose their own range:
+ * People are annoying and they often make sumb decisions. Someone could choose
+ * an extrenely large range
+ * making it next to impossible to guess. The user would thn get fustrated that
+ * they can not guess the
+ * number and stop playing the game. Choosing 1 -100 makes it so that the user
+ * can guess the number but also
+ * gives the user a bit of a challange. It is the perfect range !!
+ * 
+ */
